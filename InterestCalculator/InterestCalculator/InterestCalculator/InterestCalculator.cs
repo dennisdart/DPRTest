3b2ca29279acc13dca37941e1985f657cd02d78f
dennisdart <julianehawkins@hotmail.com>
2021-04-26 01:47:50 +0100

    Create InterestCalculator.cs

---
+++ InterestCalculator/InterestCalculator/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterestCalculator
+{
+    public class InterestCalculator:iInterestCalculator
+    {
+
+        public decimal TotalInterest(decimal amountEntered)
+        {
+            decimal interestPercentage = 0;
+            if (amountEntered < 1000)
+            {
+                interestPercentage = 1.0M;
+            }
+            else if ((amountEntered >= 1000) && (amountEntered < 5000))
+            {
+                interestPercentage = 1.5M;
+            }
+            else if ((amountEntered >= 5000) && (amountEntered < 10000))
+            {
+                interestPercentage = 2M;
+            }
+            else if ((amountEntered >= 10000) && (amountEntered < 50000))
+            {
+                interestPercentage = 2.5M;
+            }
+            else if (amountEntered >= 50000)
+            {
+                interestPercentage = 3M;
+            }
+
+            return decimal.Round((amountEntered / 100) * interestPercentage, 2);
+        }
+    }
+}
