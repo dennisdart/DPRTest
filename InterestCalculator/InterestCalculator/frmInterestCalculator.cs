550cce79ba91965a6be9b9467cdfd864b638c845
dennisdart <julianehawkins@hotmail.com>
2021-04-26 01:47:01 +0100

    Create frmInterestCalculator.cs

---
+++ InterestCalculator/InterestCalculator/frmInterestCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterestCalculator
+{
+    public partial class frmInterestCalculator : Form
+    {
+        public frmInterestCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateInterest_Click(object sender, EventArgs e)
+        {
+            InterestCalculator totalInterest = new InterestCalculator();
+            lblTotalInterest.Text = "Total Interest is " + totalInterest.TotalInterest(Convert.ToDecimal(txtEnterAmount.Text));
+        }
+    }
+}
